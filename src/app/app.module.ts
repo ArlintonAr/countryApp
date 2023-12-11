6aa00acedf1fddb68dd5aa788509ee803763b6b6
ArlintonAr <arlintonarturodelgadoflores@gmail.com>
2023-12-11 16:36:37 -0500

    Desarrollando pages countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRountingModule } from './app-routing.module';
+
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -11,9 +15,10 @@ import { AppRountingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    SharedModule,
+    HttpClientModule,
 
-    AppRountingModule
+    AppRountingModule,
+    SharedModule,
 
   ],
   providers: [],
